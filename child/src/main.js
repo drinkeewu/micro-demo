8f8d544d8ec333c10ddc97b80161a117c0116bab
drinkeewu <773119690@qq.com>
2019-12-30 09:45:57 +0800

    prefixf

---
+++ child/src/main.js
@@ -0,0 +1,31 @@
+import singleSpaVue from "single-spa-vue";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+Vue.config.productionTip = false
+const vueOptions = {
+  el: "#vue",
+  router,
+  store,
+  render: h => h(App)
+};
+if (!window.singleSpaNavigate) { // 如果不是single-spa模式
+  delete vueOptions.el;
+  new Vue(vueOptions).$mount('#vue');
+}
+
+// singleSpaVue包装一个vue微前端服务对象
+const vueLifecycles = singleSpaVue({
+  Vue,
+  appOptions: vueOptions
+});
+
+
+// 导出生命周期对象
+export const bootstrap = vueLifecycles.bootstrap; // 启动时
+export const mount = vueLifecycles.mount; // 挂载时
+export const unmount = vueLifecycles.unmount; // 卸载时
+
+export default vueLifecycles;
